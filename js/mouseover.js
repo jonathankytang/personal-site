7c95aecedf6e624dcd721da1789bbc9f0c68f726
Jonathan Tang <jonathankytang@gmail.com>
2020-06-05 17:22:11 +0100

    new site

---
+++ js/mouseover.js
@@ -0,0 +1,17 @@
+$("#dslop-img").on({
+	"mouseover" : function () {
+		this.src = "images/dslop2.jpg";
+	},
+	"mouseout" : function () {
+		this.src = "images/dslop1.jpg";
+	}
+});
+
+$("#eu28-img").on({
+	"mouseover" : function () {
+		this.src = "images/eu282.jpg";
+	},
+	"mouseout" : function () {
+		this.src = "images/eu281.jpg";
+	}
+});
